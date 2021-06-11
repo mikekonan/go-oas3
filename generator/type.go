d111c0cf4f8804874c0898a2ce616b36034c4fbc
Sergey Dobrodey <sergey.dobrodey@softswiss.com>
2021-06-11 14:44:13 +0300

    generator: support pointers in x-go-type (example: x-go-type: "*github.com/shopspring/decimal.Decimal")

--- generator/type.go
+++ generator/type.go
@@ -35,6 +35,11 @@ func (typ *Type) fillGoType(into *jen.Statement, parentTypeName string, typeName
 			into.Op("=")
 		}
 
+		if strings.HasPrefix(pkg, "*") {
+			pkg = pkg[1:]
+			into.Op("*")
+		}
+
 		into.Qual(pkg, typee)
 		return
 	}
